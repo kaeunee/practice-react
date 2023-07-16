a25850d56beb6ef360cd8697585ae6b7d00b8400
kaeun <wevyeun@gmail.com>
2023-07-16 16:09:40 +0900

    react practice

--- src/App.js
+++ src/App.js
@@ -22,11 +22,11 @@ function App() {
       </div>
 
       <button onClick={ ()=> {
-
         let copy = [...글제목];
         copy[0] = 'React 문법에 대하여';
         글제목변경(copy);
       }}>글수정</button>
+
 
       {/* <div className="list">
         <h4>{ 글제목[0] } <span onClick={()=>{ 좋아요변경(좋아요+1) }}>🩵</span> { 좋아요 } </h4>
@@ -41,31 +41,54 @@ function App() {
         <p>3월 22일 발행</p>
       </div> */}
 
-      {
+      {/* {
         글제목.map(function(a, i){
           return (
           <div className="list">
-            <h4 onClick={()=>{ setModal(true);}}>{ 글제목[i] }</h4>
+            <h4> { 글제목[i] }
+              <span onClick={()=>{
+                let copy = [...좋아요];
+                copy[i] = copy[i]+1;
+                좋아요변경(copy);
+                }}>🩵</span> {좋아요[i]} 
+            </h4>
             <p>2월 20일 발행</p>
           </div>
           )
         }) 
-      }
+      } */}
+
+
+    {
+      글제목.map(function(a,i){
+        return (
+          <div className="list" key={i}>
+            <h4 onClick={()=>{setModal(true)}}>{ 글제목[i] }
+            <span onClick={()=>{ 좋아요변경(좋아요+1)}} > 🩵</span> {좋아요}
+            </h4>
+            <p>3월 10일 발행</p>
+          </div>
+        )
+      })
+    }
 
       {
-        modal == true ? <Modal/> : null
+        modal == true ? <Modal 글제목변경={글제목변경} 글제목={글제목}/> : null
       }
 
     </div>
   );
 }
 
-function Modal(){
+function Modal(props){
   return (
     <div className="modal">
-    <h4>제목</h4>
+    <h4>{props.글제목[0]}</h4>
     <p>날짜</p>
     <p>상세내용</p>
+    <button onClick={()=>{
+      props.글제목변경(['리액트 환경', '글발행 기능', '동적인 UI']);
+    }}>글수정</button>
     </div>
   )
 }
